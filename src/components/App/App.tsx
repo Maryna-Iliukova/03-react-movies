b6902023e6df031ec32cbb2ff0a7cefc5710a68c
Maryna <marinailukova01@gmail.com>
2025-06-10 23:23:07 +0300

    fix movie

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -7,7 +7,7 @@ import Loader from '../Loader/Loader';
 import ErrorMessage from "../ErrorMessage/ErrorMessage"; 
 import MovieModal from '../MovieModal/MovieModal';
 import { fetchMovies } from "../../services/movieService";
-import type { Movie } from '../../types/movies';
+import type { Movie } from '../../types/movie';
 
 export default function App() {
   const [movies, setMovies] = useState<Movie[]>([]);
